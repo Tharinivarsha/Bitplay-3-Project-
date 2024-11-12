9b3489a1fa49fd2db1872198f7dbd58720a1a17d
Tharinivarsha <chitraswamy11@gmail.com>
2024-11-12 16:49:56 +0530

    Bitplay 3(Project)

---
+++ main.py
@@ -0,0 +1,13 @@
+def maxConsecutiveOnes(x):
+ 
+    count = 0
+    while (x!=0):
+     
+        x = (x & (x << 1))
+  
+        count=count+1
+     
+    return count
+ 
+print(maxConsecutiveOnes(14))
+print(maxConsecutiveOnes(222))
